3336b7095a70f751bc78fbda0e1126139af3ba1f
user name <user email>
2022-01-23 02:47:26 +0800

    add links to profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -96,7 +96,7 @@ class UsersController < ApplicationController
     end
     # Use callbacks to share common setup or constraints between actions.
     def set_user
-      @user = User.find(current_user.id)
+      @user = User.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
